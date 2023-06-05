a99ba6fe5e5aea2f8c5225d3ea5d7aae9715230d
Khoof_Nak <sikandernawaz35@gmail.com>
2023-06-05 15:52:37 +0500

    Adding_Scientific_Calculator

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
                     // Function to Enter Number.
 function appendNumber(num) {
-    document.getElementById("result").value += num;
+    document.getElementById("result_01").value += num;
 }
                     
                     // Function to Perform Operator. 
 function appendOperator(op) {
-    document.getElementById("result").value += op;
+    document.getElementById("result_01").value += op;
 }
 
                     // Function to Clear Screen.
 function clearResult() {
-    document.getElementById("result").value = result.value.toString().slice(0,0);
+    document.getElementById("result_01").value = "";
+    document.getElementById("result_02").value = "";
 }
 
                     // Functions to Calculate Percentage.
 function calculatePercentage() {
-    let result = document.getElementById("result").value;
+    let result = document.getElementById("result_01").value;
 
     try {
         let answer = eval(result + "/100");
-        document.getElementById("result").value = answer;
+        document.getElementById("result_02").value = answer;
     } catch (error) {
         console.log("error : " + error);
-        document.getElementById("result").value = "Masti kr rya";
+        document.getElementById("result_02").value = "Masti kr rya";
     }
 }
 
                     // Functions to Calculate Result (Eval).
 function calculate() {
-    let result = document.getElementById("result").value;
+    let result = document.getElementById("result_01").value;
     
     try {
         let answer = eval(result);
-        document.getElementById("result").value = answer;
+        document.getElementById("result_02").value = answer;
     } catch (error) {
         console.log("error : " + error);
-        document.getElementById("result").value = "Masti kr rya";
+        document.getElementById("result_02").value = "Masti kr rya";
     }
 
 }
+
+                    // Function for Toggle.
+function toggleMode() {
+    const body = document.querySelector('body');
+    body.classList.toggle('scientific-calculator');
+                        
+    const toggleButton = document.getElementById('toggle-button');
+    if (body.classList.contains('scientific-calculator')) {
+        document.getElementsByClassName('scientific-calculator').remove(style.display);
+    } else {
+        toggleButton.body.classList = 'button-section';
+    }
+}
+
+// [ 
+//     let result1 = document.getElementById('result_01');
+//         result1.style.height = '70px';
+//     let result2 = document.getElementById('result_02');
+//         result2.style.height = '70px';
+// ]
