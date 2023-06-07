0d8a827a4f33bcb088a2285481dce46488103f2b
Khoof_Nak <sikandernawaz35@gmail.com>
2023-06-07 22:02:13 +0500

    Adding_Functions

--- script.js
+++ script.js
@@ -19,6 +19,63 @@ function delResult() {
     document.getElementById('result_01').value = document.getElementById('result_01').value.slice(0, -1);
 }
 
+                    // Function to Calculate Square Root.
+function sqrRoot() {
+    document.getElementById("result_01").value = Math.sqrt(result.value).toFixed(4);
+}
+
+                    // Function to Calculate Log.
+function calculateLog() {
+    let result = document.getElementById("result_01").value;
+
+    try {
+        let answer = eval(result + 'Math.log10');
+        document.getElementById("result_02").value = answer;
+    } catch (error) {
+        console.log("error : " + error);
+        document.getElementById("result_02").value = "Masti kr rya";
+    }
+}
+
+                    // Function to Calculate Ln.
+function calculateLn() {
+    document.getElementById("result").value = Math.log(result.value).toFixed(4);
+}
+
+                    // Alert and Value.
+function valueOfSin() {
+    appendNumber('Sin(');
+    alert("Please make sure to close the bracket after entering the value.");
+}
+
+                    // Functions to Calculate Trigonometric Values. 
+function calculateSin() {
+    let result1 = document.getElementById('result_01').value;
+    let slice = result1.slice(4, -1);
+    let sin = Math.sin(slice * (Math.PI / 180));
+    return sin;
+}
+function calculateCos() {
+    
+}
+function calculateTan() {
+    
+}
+
+                    // Functions to Calculate Inverse Trigonometric Values.
+function calculateSinInverse() {
+    let result1 = document.getElementById('result_01').value;
+    let slice = result1.slice(4, -1);
+    let sinInverse = 1 / (1 / (Math.sin(slice * (Math.PI / 180))));
+    return sinInverse;
+}
+function calculateCosInverse() {
+   
+}
+function calculateTanInverse() {
+    
+}
+
 
                     // Functions to Calculate Percentage.
 function calculatePercentage() {
@@ -33,30 +90,16 @@ function calculatePercentage() {
     }
 }
 
-                    // Functions to Calculate Result (Eval).
-function calculate() {
-    let result = document.getElementById("result_01").value;
-    
-    try {
-        let answer = eval(result);
-        document.getElementById("result_02").value = answer;
-    } catch (error) {
-        console.log("error : " + error);
-        document.getElementById("result_02").value = "Masti kr rya";
-    }
 
-}
-
-                    // Function for Toggle.
+// Function for Toggle.
 function slider() {
     let scientificCalc = document.querySelector('.scientific-calculator');
     let buttonSection = document.querySelector('.button-section');
     let img = document.querySelector('.nav-bar img');
     let result1 = document.getElementById('result_01');
     let result2 = document.getElementById('result_02');
-
+    
     if (scientificCalc.classList.contains('hidden')) {
-        // Show scientific calculator and change the image
         scientificCalc.classList.remove('hidden');
         buttonSection.classList.add('hidden');
         img.src = './assests/converter_01.png';
@@ -64,7 +107,6 @@ function slider() {
         result1.style.height = '81px';
         result2.style.height = '81px';
     } else {
-        // Show button section and change the image back
         buttonSection.classList.remove('hidden');
         scientificCalc.classList.add('hidden');
         img.src = './assests/converter.png';
@@ -72,4 +114,18 @@ function slider() {
         result1.style.height = '120px';
         result2.style.height = '120px';
     }
+}
+
+                        // Functions to Calculate Result (Eval).
+function calculate() {
+let result = document.getElementById("result_01").value;
+
+try {
+let answer = eval(result);
+document.getElementById("result_02").value = answer;
+} catch (error) {
+console.log("error : " + error);
+document.getElementById("result_02").value = "Masti kr rya";
+}
+
 }
